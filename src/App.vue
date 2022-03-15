0c7630c31d341ca8704005de8fcbd55dcc56743d
Victor Fernandez <victorhf2014@gmail.com>
2022-03-14 22:56:58 -0300

    todolist

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,70 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container">
+      <div class="row">
+        <div class="vol-12">
+          <h1 class="display-1 my-5">Crear Una Nueva lista de Tarea</h1>
+
+          <div class="d-flex">
+            <label for="nombre">Tarea</label>
+            <input
+              type="text"
+              placeholder="nueva tarea"
+              class="form-control"
+              v-model="agregarTarea"
+              @keyup.enter="agregoNew"
+            />
+            <button
+              v-
+              class="mx-1 btn btn-outline-secondary"
+              type="button"
+              v-on:click="agregoTarea"
+            >
+              Crear
+            </button>
+          </div>
+          <div class="col-12">
+            <h2 class="my-2">Lista</h2>
+            <ul>
+              <li
+                class="list-group-item my-2"
+                v-for="(tarea1, i) in tareaFinal"
+                :key="i"
+              >
+                {{ tarea1 }}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      tareaFinal: [],
+      agregarTarea: "",
+    };
+  },
+  methods: {
+    agregoTarea() {
+      this.agregarTarea === ""
+        ? alert("crear tarea")
+        : this.tareaFinal.push(this.agregarTarea);
+      this.agregarTarea = "";
+    },
   },
 };
+// data() {
+//   return {
+//     show: true,
+//   };
+// },
+//};
 </script>
 
 <style>
